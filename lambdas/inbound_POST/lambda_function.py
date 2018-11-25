ee170a548e396f21a939d587cbcf6df54b6fa176
Alexander Laird <alaird@twilio.com>
2018-11-24 22:30:20 -0600

    no message

--- lambdas/inbound_POST/lambda_function.py
+++ lambdas/inbound_POST/lambda_function.py
@@ -6,6 +6,7 @@ from urllib import parse
 from botocore.vendored import requests
 
 WILDFIRE_AQI_API_URL = os.environ.get("WILDFIRE_AQI_API_URL")
+WILDFIRE_EVACUATION_API_URL = os.environ.get("WILDFIRE_EVACUATION_API_URL")
 WILDFIRE_FIRE_API_URL = os.environ.get("WILDFIRE_FIRE_API_URL")
 
 AQI_MESSAGES = {
