293df035ee36879a0f859ff41978d6a25a19ff6a
Alex Laird <alexdlaird@gmail.com>
2018-12-10 17:16:19 -0800

    Datadog metrics need to be printed rather than logged.

--- utils/metricutils.py
+++ utils/metricutils.py
@@ -4,8 +4,10 @@ import time
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def log(value, type, metric, tags=[]):
-    logger.info("MONITORING|{}|{}|{}|airqualitybot.{}|#{}".format(int(time.time()), value, type, metric, ",".join(tags)))
+def log(value, type, metric, tags=None):
+    if tags is None:
+        tags = []
+    print("MONITORING|{}|{}|{}|{}|#{}".format(int(time.time()), value, type, "airqualitybot." + metric, ",".join(tags)))
 
 def increment(metric, tags=[]):
     log(1, "count", metric, tags)
